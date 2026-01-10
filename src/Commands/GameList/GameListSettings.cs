0dcefba0b883bb14ee104ea550843c24c8862844
nicholasVilela <vilela.freelance@gmail.com>
2026-01-10 00:37:16 -0500

    Updated Titles for GameList and Metadata Settings

--- src/Commands/GameList/GameListSettings.cs
+++ src/Commands/GameList/GameListSettings.cs
@@ -4,7 +4,7 @@ using Spectre.Console.Cli;
 namespace Vault.Commands;
 
 public class GameListSettings : BaseSettings {
-  public override string Title => "Info";
+  public override string Title => "GameList";
   public override string ReadPath => @$"{Path}{Console}\ROMS";
   public override string DefaultDestination => @$"{Path}{Console}";
 
